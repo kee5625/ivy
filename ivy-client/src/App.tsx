fcf33b8f1cd94109f44135e0cfe0b0478538aa53
sharvanr <155874363+sharvanr@users.noreply.github.com>
2026-03-06 07:09:05 +0000

    adding test hero page

--- ivy-client/src/App.tsx
+++ ivy-client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import JobDetailsPage from "./pages/graph/graph";
+import Hero from "./pages/hero";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/graph/:jobId" element={<JobDetailsPage />} />
+        <Route path="/test" element={<Hero />} />
       </Routes>
     </BrowserRouter>
   );
