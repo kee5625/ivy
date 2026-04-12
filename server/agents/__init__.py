9ac1acc3986629fa4fd0e4361675c1aad6b8dc3e
Karthik R <karthikeya.rachamolla@gmail.com>
2026-04-12 19:28:23 +0530

    uv init

--- server/agents/__init__.py
+++
@@ -1,5 +0,0 @@
-from agents.ingestion_agent import IngestionAgent
-from agents.plot_hole_agent import PlotHoleAgent
-from agents.timeline_agent import TimelineAgent
-
-__all__ = ["IngestionAgent", "TimelineAgent", "PlotHoleAgent"]
--- server/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from services import parse_service, timeline_service
-
-__all__ = ["parse_service", "timeline_service"]
