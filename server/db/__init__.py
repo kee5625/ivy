ba841fbaa07953cffde5bb963913a27e195e75fe
Karthik R <karthikeya.rachamolla@gmail.com>
2026-04-24 12:16:18 +0530

    updating init

--- server/db/__init__.py
+++ server/db/__init__.py
@@ -1,6 +1,7 @@
-"""Database module for NeonDB connection pooling."""
+"""Database module for NeonDB connection pooling and Redis."""
 
 from db.connection import get_pool, init_pool, close_pool, get_conn
+from db.redis import get_redis, init_redis, close_redis, job_status_key, JOB_TTL_SECONDS
 from db.repository import JobRepository, ChapterRepository, TimelineRepository
 
 __all__ = [
@@ -8,6 +9,11 @@ __all__ = [
     "init_pool",
     "close_pool",
     "get_conn",
+    "get_redis",
+    "init_redis",
+    "close_redis",
+    "job_status_key",
+    "JOB_TTL_SECONDS",
     "JobRepository",
     "ChapterRepository",
     "TimelineRepository",
